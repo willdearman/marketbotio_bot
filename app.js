78456143d815dd09788408982248036cd383c40b
Will Dearman <will@willdearman.com>
2016-08-26 16:28:48 -0500

    Updated BotBuilder libraries with latest from Github. Also app.js working.

--- app.js
+++ app.js
@@ -1,39 +1,25 @@
-// Starting file. Run using ```node app.js```
-
-// # Requirements
-var restify = require('restify');
+/* Starting file. Run using ```node app.js``` */
 var builder = require('botbuilder');
+var restify = require('restify');
 
-// # Define Connector
-
+// # Top Level Bot Dialogue
 var connector = new builder.ChatConnector({
-    //appId: "NULL",
-    //appPassword: "NULL"
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-
-// # Bot Dialogue
 var bot = new builder.UniversalBot(connector);
 bot.dialog('/', [
     function (session) {
-        session.send('I am alive')
+        session.beginDialog('/askName');
     },
+    function (session, results) {
+        session.send('Hello %s!', results.response);
+    }
+]);
+bot.dialog('/askName', [
     function (session) {
-        builder.Prompts.text(session, "Hello... What's your name?");
-    },
-    function (session, results) {
-        session.userData.name = results.response;
-        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
-    },
-    function (session, results) {
-        session.userData.coding = results.response;
-        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
-    },
-    function (session, results) {
-        session.userData.language = results.response.entity;
-        session.send("Got it... " + session.userData.name + 
-                     " you've been programming for " + session.userData.coding + 
-                     " years and use " + session.userData.language + ".");
+        builder.Prompts.text(session, 'Hi! What is your name?');
     }
 ]);
 
@@ -41,5 +27,6 @@ bot.dialog('/', [
 var server = restify.createServer();
 server.post('/api/messages', connector.listen());
 server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
+server.post('/api/messages', connector.listen());
